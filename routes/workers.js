cf74d7719498fc2f80b69049cc61a0737c52d2f6
buckle90 <buckle90@msu.edu>
2017-11-30 19:34:46 -0800

    Changes

--- routes/workers.js
+++ routes/workers.js
@@ -3,11 +3,13 @@ let router = express.Router();
 let https = require('https');
 
 let url = "https://api.workday.com/common/v1/workers";
-
+let token = '';
 
 /* GET users listing. */
 router.post('/', function (req, res) {
+    process.stdout.write(req.body);
     res.set('Content-Type', 'application/json');
+    res.set('Authorization', 'bearer ' + token);
 
     https.get(url, (result) => {
         console.log('statusCode:', result.statusCode);
@@ -23,6 +25,7 @@ router.post('/', function (req, res) {
         console.error(e);
     });
 
+    process.stdout.write("TEST");
 });
 
 let makeWebhookResult = function () {
