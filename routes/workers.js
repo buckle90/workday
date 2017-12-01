0aedf10d603869405fbeec2ea469c57def75900e
buckle90 <buckle90@msu.edu>
2017-11-30 19:13:14 -0800

    Changes

--- routes/workers.js
+++ routes/workers.js
@@ -2,12 +2,14 @@ let express = require('express');
 let router = express.Router();
 let https = require('https');
 
+let url = "https://auth.api.workday.com/v1/authorize?response_type=code&client_id=OWE0MjExMzMtZTk3My00YTNmLTgzMmMtZDE2YWE4OTY3ODg1&state=xyz&redirect_uri=https://oauth-redirect.googleusercontent.com/r/workday-4d484";
+
 
 /* GET users listing. */
 router.post('/', function (req, res) {
     res.set('Content-Type', 'application/json');
 
-    https.get('https://encrypted.google.com/', (res) => {
+    https.get(url, (res) => {
         console.log('statusCode:', res.statusCode);
         console.log('headers:', res.headers);
 
