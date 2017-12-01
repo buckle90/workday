94ca1d0c8cc9cdca96e9b422501bfa9eb4547c41
buckle90 <buckle90@msu.edu>
2017-11-30 19:17:39 -0800

    Changes

--- routes/workers.js
+++ routes/workers.js
@@ -2,26 +2,27 @@ let express = require('express');
 let router = express.Router();
 let https = require('https');
 
-let url = "https://auth.api.workday.com/v1/authorize?response_type=code&client_id=OWE0MjExMzMtZTk3My00YTNmLTgzMmMtZDE2YWE4OTY3ODg1&state=xyz&redirect_uri=https://oauth-redirect.googleusercontent.com/r/workday-4d484";
+let url = "https://api.workday.com/common/v1/workers";
 
 
 /* GET users listing. */
 router.post('/', function (req, res) {
     res.set('Content-Type', 'application/json');
 
-    https.get(url, (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
+    https.get(url, (result) => {
+        console.log('statusCode:', result.statusCode);
+        console.log('headers:', result.headers);
 
-        res.on('data', (d) => {
+        result.on('data', (d) => {
             process.stdout.write(d);
+
+            res.send(d);
         });
 
     }).on('error', (e) => {
         console.error(e);
     });
 
-    res.send(makeWebhookResult());
 });
 
 let makeWebhookResult = function () {
