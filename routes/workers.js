785cfc156ead8bcce301923f9a4816030ffde82f
buckle90 <buckle90@msu.edu>
2017-11-30 18:05:53 -0800

    Changes

--- routes/workers.js
+++ routes/workers.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+
 
 /* GET users listing. */
 router.post('/', function (req, res) {
-      res.set('Content-Type', 'application/json');
-      res.send(makeWebhookResult());
+    res.set('Content-Type', 'application/json');
+
+    https.get('https://encrypted.google.com/', (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+
+    }).on('error', (e) => {
+        console.error(e);
+    });
+
+    res.send(makeWebhookResult());
 });
 
 var makeWebhookResult = function () {
